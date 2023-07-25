7fd7c19a90224fbc23aacbd8e7ebdc69f97244d6
zokkyen <stephane.swaenepoel@wanadoo.fr>
2023-07-25 16:11:35 +0200

    add employee login + change logo and title of app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,22 @@
-import './App.css';
-import RouteConfig from 'routes';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { Button, Card, ThemeProvider } from '@mui/material';
-import icon from '../../assets/icon.svg';
+import React from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import './App.css';
 import defaultTheme from './utils/theming/theme';
-import CustomDrawer from './components/CustomDrawer';
+import RouteConfig from '../routes';
+import { UserProvider } from './utils/UserContext';
 
 export default function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Router>
-        <RouteConfig />
-      </Router>
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <Router>
+          <RouteConfig />
+        </Router>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
--- src/renderer/drawer/HomeDrawer.jsx
+++ src/renderer/drawer/HomeDrawer.jsx
@@ -1,25 +1,18 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
+import React, { useContext } from 'react';
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import icon from '../../../assets/icon.png';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
@@ -28,12 +21,15 @@ import arrayNavigation from 'renderer/drawerNavigation';
 import Main from 'component/Main';
 import AppBar from 'component/AppBar';
 import DrawerHeader from 'component/DrawerHeader';
+import { useNavigate } from 'react-router-dom';
+import icon from '../../../assets/icon.png';
+import { UserContext } from '../utils/UserContext';
 
 const drawerWidth = 240;
 
-
-const HomeDrawer = () => {
-
+function HomeDrawer() {
+  const { user, logOut } = useContext(UserContext);
+  const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const openMenu = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -60,11 +56,21 @@ const HomeDrawer = () => {
     setSelectedItem(item);
   };
 
+  const handleLogout = () => {
+    logOut();
+    navigate('/');
+  };
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar style={{backgroundColor: '#212830', color :"#FFF"}} position="fixed" open={open} drawerWidth={drawerWidth}>
-        <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
+      <AppBar
+        style={{ backgroundColor: '#212830', color: '#FFF' }}
+        position="fixed"
+        open={open}
+        drawerWidth={drawerWidth}
+      >
+        <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -74,62 +80,73 @@ const HomeDrawer = () => {
           >
             <MenuOpenIcon />
           </IconButton>
-          <img style={{width: '80px'}} src={icon} alt="Logo" />
+          <img style={{ width: '80px' }} src={icon} alt="Logo" />
           <Button
-            style={{color: 'white', backgroundColor: '#212830'}}
+            style={{ color: 'white', backgroundColor: '#212830' }}
             id="demo-positioned-button"
             aria-controls={openMenu ? 'demo-positioned-menu' : undefined}
             aria-haspopup="true"
             aria-expanded={openMenu ? 'true' : undefined}
             onClick={handleClick}
           >
-          Levert M-A <KeyboardArrowDownIcon/>
-        </Button>
-      <Menu
-      style={{color: '#212830'}}
-        id="demo-positioned-menu"
-        aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={openMenu}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+            {user.lastname} {user.firstname}
+            <KeyboardArrowDownIcon />
+          </Button>
+          <Menu
+            style={{ color: '#212830' }}
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={openMenu}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <MenuItem onClick={handleClose}>Mon profil</MenuItem>
+            <MenuItem onClick={handleClose}>Mon compte</MenuItem>
+            <MenuItem onClick={handleLogout}>Deconnexion</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
-      <Drawer 
+      <Drawer
         sx={{
           width: drawerWidth,
           flexShrink: 0,
           '& .MuiDrawer-paper': {
             width: drawerWidth,
-            backgroundColor: "#212830",
+            backgroundColor: '#212830',
           },
         }}
         variant="persistent"
         anchor="left"
         open={open}
       >
-        <DrawerHeader style={{backgroundColor: '#212830'}}>
-          <IconButton style={{ color: '#FFFF'}} onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        <DrawerHeader style={{ backgroundColor: '#212830' }}>
+          <IconButton style={{ color: '#FFFF' }} onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </DrawerHeader>
-        <List style={{backgroundColor: '#212830', color: '#FFFF'}}>
-          {arrayNavigation.map((item, index) => (
-            <ListItem key={index} disablePadding>
-              <ListItemButton style={{backgroundColor: item === selectedItem ? '#f6a400' : '#212830'}} onClick={() => handleItemClick(item)}>
-                <ListItemIcon style={{ color: '#FFFF'}}>
+        <List style={{ backgroundColor: '#212830', color: '#FFFF' }}>
+          {arrayNavigation.map((item) => (
+            <ListItem key={item.id} disablePadding>
+              <ListItemButton
+                style={{
+                  backgroundColor:
+                    item === selectedItem ? '#f6a400' : '#212830',
+                }}
+                onClick={() => handleItemClick(item)}
+              >
+                <ListItemIcon style={{ color: '#FFFF' }}>
                   {item.icon}
                 </ListItemIcon>
                 <ListItemText primary={item.label} />
@@ -145,4 +162,4 @@ const HomeDrawer = () => {
   );
 }
 
-export default HomeDrawer
+export default HomeDrawer;
